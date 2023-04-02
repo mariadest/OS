c850ef814e597d4c83e4af66de71f9db0e52398f
Maria Desteffani <maria.desteffani@unibas.ch>
2023-04-02 13:12:36 +0200

    sheet fertig

--- Sheet 2/Maria/T4.c
+++ Sheet 2/Maria/T4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #define SIZE 5
 struct process{
@@ -14,7 +15,14 @@ void init_processes(struct process *);
 void display(struct process *);
 void display_average_waiting_time(struct process *);
 void schedule_FCFS(struct process *);
+void schedule_SJF(struct process *);
+void schedule_LJF(struct process *);
 
+
+
+// ------------------------------------------------
+//         MAIN METHOD
+// ------------------------------------------------
 int main()
 {
 	struct process processes[SIZE];	
@@ -22,13 +30,31 @@ int main()
 
 	printf("Orignial input (processes)\n");
 	display(processes);
-	
+
+	printf("\n");
 	schedule_FCFS(processes);
 	printf("Execution according to FCFS policy\n");
 	display(processes);
 	display_average_waiting_time(processes);	
 
+	printf("\n");
+	schedule_SJF(processes);
+	printf("Execution according to SJF policy\n");
+	display(processes);
+	display_average_waiting_time(processes);	
+
+	printf("\n");
+	schedule_LJF(processes);
+	printf("Execution according to LJF policy\n");
+	display(processes);
+	display_average_waiting_time(processes);	
+	
+
 }
+// ------------------------------------------------
+
+
+// initializes the struct process
 void init_processes(struct process * input)
 {
 	srand(100);
@@ -40,6 +66,8 @@ void init_processes(struct process * input)
 		input[i].start_time=-1;      
 	}	
 }
+
+// displays the struct process
 void display(struct process * input){
 	printf("process<id, arr, exe, start>:");
 	for(int i=0;i<SIZE;i++)
@@ -50,23 +78,86 @@ void display(struct process * input){
 	printf("\n");
 }
 
+// schedules according to FCFS
 void schedule_FCFS(struct process * arr)
 {
-	//TODO
-	printf("Not implemented Yet!\n");
+	// calculates the start_time of each process by giving the first process to arrive a start_time of 0
+	// all following processes get the execution_time of those before added to their start_time
+	int startTime = 0;
+	for (int i = 0; i < SIZE; i++){
+		arr[i].start_time = startTime;
+		startTime += arr[i].execution_time;
+	}
+
 }
+
+// schedules the processes according to which job is the fastest
 void schedule_SJF(struct process * arr)
 {
-        //TODO
-        printf("Not implemented Yet!\n");
+	int shortestTime = INT_MAX;	// assuming no process is longer than that
+	int shortestJob;
+	int startingTime = 0;
+
+	// reseting starting times
+	// this is technically not needed but it allows to run several scheduling methods in the main method without recompiling
+	for (int i = 0; i < SIZE; i++) {
+		arr[i].start_time = -1;
+	}
+
+	// goes over all processes
+	for (int j = 0; j < SIZE; j++) {
+		// gets the shortest process which hasn't been assigned a starting time
+		for (int i = 0; i < SIZE; i++) {
+			if (arr[i].execution_time <= shortestTime && arr[i].start_time == -1) {
+				shortestTime = arr[i].execution_time;
+				shortestJob = i;
+			}
+		}
+
+		arr[shortestJob].start_time = startingTime;			// sets the start time of the chosen process
+		startingTime += arr[shortestJob].execution_time;	// updates the general starting time
+		shortestTime = INT_MAX;								// resets variable to check for the shortest execution time
+	}
 }
+
+// schedules the processes according to which job takes the longest
 void schedule_LJF(struct process * arr)
 {
-        //TODO
-        printf("Not implemented Yet!\n");
+    int longestJob;
+	int longestTime = 0;
+	int startingTime = 0;
+
+	// reseting starting times
+	// this is technically not needed but it allows to run several scheduling methods in the main method without recompiling
+	for (int i = 0; i < SIZE; i++) {
+		arr[i].start_time = -1;
+	}
+
+	// goes over all processes
+	for (int j = 0; j < SIZE; j++) {
+		// gets the longest process which hasn't been assigned a starting time
+		for (int i = 0; i < SIZE; i++) {
+			if (arr[i].execution_time >= longestTime && arr[i].start_time == -1) {
+				longestTime = arr[i].execution_time;
+				longestJob = i;
+			}
+		}
+
+		arr[longestJob].start_time = startingTime;			// sets the start time of the chosen process
+		startingTime += arr[longestJob].execution_time;		// updates the general starting time
+		longestTime = 0;									// resets variable to check for the longest execution time
+	}
 }
+
 void display_average_waiting_time(struct process * arr)
 {
-	//TODO
-	printf("Not implemented Yet!\n");
+	// calculates the average waiting time by adding up all start times and then dividing by the amount of processes
+	float avgWaitingTime = 0;
+	for (int i = 0; i < SIZE; i++) {
+		avgWaitingTime += arr[i].start_time;
+	}
+	avgWaitingTime = avgWaitingTime / SIZE;
+	printf("-------------------------------\n");
+	printf("| Average waiting time: %2.2fs |\n", avgWaitingTime);
+	printf("-------------------------------\n");
 }
